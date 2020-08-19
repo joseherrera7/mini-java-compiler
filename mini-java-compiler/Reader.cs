c443cf6a12e41b8ecb3d7b16809762c1aa98e1e9
joseherrera7 <jcherrera98@gmail.com>
2020-08-18 18:29:52 -0600

    Agregando identificadores, reservadas, operadores a tokens, tambien poniendo sistema para controlar columnas

--- mini-java-compiler/Reader.cs
+++ mini-java-compiler/Reader.cs
@@ -12,19 +12,24 @@ namespace mini_java_compiler
     class Reader
     {
         //INCLUÍR MÁS OPERADORES SI ME HIZO FALTA ALGUNO
-        string[] operadores = { "|", "*", "+", "?", "•", "(", ")" };
+        string[] operadores = { "+","-","*", "/", "%", "<", "<=", ">", ">=", "=", "==", "!=", "&&", "||", "!", ";", ",", ".", "[", "]", 
+            "(", ")", "{", "}",
+    "[]", "()", "{}" };
 
         //LLEVA EL CONTROL SI ACTION CONTIENE LLAVE DE APERTURA Y CIERRE
         int contReservadas = 1;
         bool actionLlaves = false;
+        int columnCounter = 0;
         //GUARDA EL ERROR
         public int ERROR = 0;
         //VERIFICA SI SE LEYÓ LA SECCIÓN ERROR
         bool bError = false;
+        string writer = "";
 
-
+        Dictionary<string, string> tokens = new Dictionary<string, string>();
         Hashtable hashtable = new Hashtable();
         char[] charsToTrim = { '\t', ' ', '\n', '\r' };
+        
         string[] reserved = { "void", "int", "double", "boolean",
                 "string", "class", "const", "interface", "null", "this",
                 "extends", "implements", "for", "while", "if", "else",
@@ -37,7 +42,6 @@ namespace mini_java_compiler
                 "synchronized", "throw", "throws", "transient", "try", "volatile",
                 "true", "false", "var", "goto", "const"
     };
-        List<String> tokens = new List<string>();
         
         /// <summary>
         /// Read Line per line the file, and this calls another methods to confirm
@@ -45,17 +49,68 @@ namespace mini_java_compiler
         /// <param name="line">the line</param>
         public void ReadProgram(string line)
         {
+            columnCounter = 1;
+            int columnStart = 0;
+            int columnEnd = 0;
             string buffer = "";
             foreach (var c in line)
             {
                 if (Char.IsWhiteSpace(c) || c.Equals(';')) // ignore blankspaces
                 {
-                    Segmentation(buffer); //call segmentacion to return into the switch
-                    tokens.Add(buffer);
+                    columnEnd = columnCounter-1;
+                    idOrReserved(buffer);
                     buffer = "";
+                }  
+                else if (operadores.Contains(c.ToString()) || operadores.Contains(buffer))
+                {
+                    if (operadores.Contains(buffer))
+                    {
+                        if (operadores.Contains(buffer+c))
+                        {
+                            if (buffer.Contains(""))
+                            {
+                                columnStart = columnCounter;
+                                buffer += c;
+                            }
+                            else
+                            {
+                                buffer += c;
+                            }
+                        }
+                        else
+                        {
+                            addOperator(buffer);
+                            buffer = c.ToString();
+                            columnEnd = columnCounter-1;
+                        }
+                    }
+                    else
+                    {
+                        if (buffer.Equals(""))
+                        {
+                            columnStart = columnCounter;
+                            buffer = c.ToString();
+                        }
+                        else
+                        {
+                            idOrReserved(buffer);
+                            buffer = c.ToString();
+                        }
+                    }
                 }
-                buffer += c;
-                
+                else
+                {
+                    if (buffer.Equals(""))
+                    {
+                        buffer += c;
+                        columnStart = columnCounter;
+                    } 
+                    else
+                    {
+                        buffer += c;
+                    }
+                }
+                columnCounter++;
             }
         }
         
@@ -66,10 +121,45 @@ namespace mini_java_compiler
         /// <returns></returns>
         private int Segmentation(string buffer)
         {
-            string idPattern = "";
-            string anotherPattern = "";
-            //if (buffer)
-            return 0;
+            var idPattern = "^[A-Za-z_$]{1}[a-zA-Z\\d$_]*$";
+            
+            if (reserved.Contains(buffer)) return 1;
+            else
+            {
+                if (Regex.IsMatch(buffer, idPattern))
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
         }
+
+        private void idOrReserved(string buffer)
+        {
+            var bufferIs = Segmentation(buffer); //call segmentacion to return into the switch para ver si es reservada o id
+            switch (bufferIs)
+            {
+                case 0:
+                    tokens.Add(buffer,"ERROR");
+                    break;
+                case 1:
+                    tokens.Add(buffer, "RESERVADA");
+                    break;
+                case 2:
+                    tokens.Add(buffer, "IDENTIFICADOR");
+                    break;
+            }
+        }
+
+        private void addOperator(string buffer)
+        {
+            //code to add operator
+            tokens.Add(buffer, "OPERADOR");
+        }
+        string blockComments = @"/\*(.*?)\*/";
+        string lineComments = @"//(.*?)\r?\n";
     }
 }
