d96b253e9fb4dde7c6dd5b0bd3b762b3ed29896b
Renato <renatojose98@yahoo.com>
2020-09-05 19:09:30 -0600

    Análisis sintáctico descendente recursivo
    Primera prueba con:
    
    // EXPR := Number {Operator Number}
    // Operador   := "+" | "-"
    // Numero     := Digit{Digit}
    // Digito      := "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"

---
+++ mini-java-compiler/Token.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_java_compiler
+{
+        public abstract class Token
+        {
+        }
+
+        public class OperatorToken : Token
+        {
+
+        }
+        public class PlusToken : OperatorToken
+        {
+        }
+
+        public class MinusToken : OperatorToken
+        {
+        }
+
+        public class NumberConstantToken : Token
+        {
+            private readonly int _value;
+
+            public NumberConstantToken(int value)
+            {
+                _value = value;
+            }
+
+            public int Value
+            {
+                get { return _value; }
+            }
+        }
+    }
+
+
